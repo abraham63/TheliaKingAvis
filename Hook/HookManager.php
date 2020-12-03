6fcc2daefb0d1e63a37df5922661c10c602b9909
Kevin Seebach <kevincbar@gmail.com>
2020-12-03 08:59:02 +0100

    Update HookManager.php

--- Hook/HookManager.php
+++ Hook/HookManager.php
@@ -11,15 +11,11 @@ class HookManager extends BaseHook
 
     public function displayKAWidget(HookRenderEvent $event)
     {
-				$ka_id = ConfigQuery::create()->findOneByName('tka_marchand_id')->getValue();
-				$ka_token = ConfigQuery::create()->findOneByName('tka_marchand_token')->getValue();
-
-    		if(ConfigQuery::create()->findOneByName('tka_display_tka_widget')->getValue() == "true"){
-            $html = $this->render("tkawidget.html", array("tka_id" => $ka_id, "tka_token" => $ka_token));
-      			$event->add($html);
-          }
+    	if(ConfigQuery::create()->findOneByName('tka_display_tka_widget')->getValue() == "true"){
+		$ka_id = ConfigQuery::create()->findOneByName('tka_marchand_id')->getValue();
+		$ka_token = ConfigQuery::create()->findOneByName('tka_marchand_token')->getValue();
+        	$html = $this->render("tkawidget.html", array("tka_id" => $ka_id, "tka_token" => $ka_token));
+      		$event->add($html);
+        }
     }
-
-
-
 }
